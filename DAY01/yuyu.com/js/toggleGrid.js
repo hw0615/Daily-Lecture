fe5753e6c0a0c88b8f98b8cad8c0cb54fc140e5b
hw0615 <medianhw@gmail.com>
2017-05-24 18:41:50 +0900

    ~Day03

---
+++ DAY01/yuyu.com/js/toggleGrid.js
@@ -0,0 +1,12 @@
+/* toggleGrid.js @ 2017, yamoo9.net */
+
+var doc = document;
+var body = doc.body;
+
+doc.addEventListener('keyup', toggleGrid);
+
+function toggleGrid(e) {
+  if (e.keyCode === 71 && e.shiftKey ) {
+    body.classList.toggle('show-grid');
+  }
+}
